c3c5ce3e695048b26493f7a145e5022a1d8b2d49
unknown <vunge7.1990@gmail.com>
2024-06-06 18:13:48 +0100

    first commit

---
+++ src/main/java/com/meudominio/meu_primeiro_projecto/MeuPrimeiroProjectoApplication.java
@@ -0,0 +1,13 @@
+package com.meudominio.meu_primeiro_projecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuPrimeiroProjectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuPrimeiroProjectoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meudominio/meu_primeiro_projecto/MeuPrimeiroProjectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meudominio.meu_primeiro_projecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuPrimeiroProjectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
